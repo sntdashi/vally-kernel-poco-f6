fb76a2d19d44078e91700696ec17a16e2a4baf64
Rauzzan Muhamad Fikri <142691671+sntdashi@users.noreply.github.com>
2026-03-16 07:20:20 +0700

    Enhance build script with new tools and options
    Updated build script to include additional build tools and parameters.

--- scripts/build.sh
+++ scripts/build.sh
@@ -57,8 +57,15 @@ make -j$(nproc --all) O=out \
 ARCH=arm64 \
 CC=clang \
 LD=ld.lld \
+AR=llvm-ar \
+NM=llvm-nm \
+OBJCOPY=llvm-objcopy \
+OBJDUMP=llvm-objdump \
+STRIP=llvm-strip \
 LLVM=1 \
-LLVM_IAS=1
+LLVM_IAS=1 \
+HOSTCC=gcc \
+HOSTCXX=g++
 
 echo "===== COPY KERNEL ====="
 
